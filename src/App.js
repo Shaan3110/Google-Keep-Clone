c107e36ef9b70e41bdc39ef412b591268e80bf18
Shaan3110 <hitarasigan0987654321@gmail.com>
2021-08-08 23:08:13 +0530

    fixed the issue for hosting issue

--- src/App.js
+++ src/App.js
@@ -1,49 +1,17 @@
 import './App.css';
-import Header from './MyComponents/Header';
-// import TextArea from './MyComponents/TextArea';
-import CreateNote from './MyComponents/CreateNote';
-import Note from './MyComponents/Note';
-import Footer from './MyComponents/Footer';
-import React,{useState} from 'react';
+import React from 'react';
+import {Switch,Route} from 'react-router-dom'
+import MainPage from './MyComponents/MainPage';
 
 function App() 
 {
-  const [array, setarray] = useState([])
-  const addElement=(note)=>{
-    setarray((elements)=>{
-      return(
-        [...elements,
-        note]
-      )
-    })
-  }
-  const del=(id)=>{
-    alert("deleting!");
-    setarray((olddata)=>{
-      return(
-      olddata.filter((values,indx)=>{
-            return indx!==id;
-        })
-      )
-      })
-    // console.log(array);
-  }
-  // console.log(Expand);
   return (
     <>
-      <Header/>
+      <Switch>
+        <Route exact path='/Google-Keep-Clone' component={MainPage}/>
+      </Switch>
+      
       {/* <TextArea onClick={expand}/> */}
-      <CreateNote addElement={addElement}/>
-      <div className='notes'>
-      {
-        array.map((ele,index)=>{
-          return(
-            <Note key={index} ele={ele} id={index} del={del}/>
-          )
-        })
-      }
-      </div>
-      <Footer/>
     </>
   );
 }
---
+++ src/MyComponents/MainPage.jsx
@@ -0,0 +1,47 @@
+import React,{useState} from 'react'
+import Header from './Header';
+// import TextArea from './MyComponents/TextArea';
+import CreateNote from './CreateNote';
+import Note from './Note';
+import Footer from './Footer';
+const MainPage = () => {
+    const [array, setarray] = useState([])
+    const addElement = (note) => {
+        setarray((elements) => {
+            return (
+                [...elements,
+                    note]
+            )
+        })
+    }
+    const del = (id) => {
+        // alert("deleting!");
+        setarray((olddata) => {
+            return (
+                olddata.filter((values, indx) => {
+                    return indx !== id;
+                })
+            )
+        })
+        // console.log(array);
+    }
+    // console.log(Expand);
+    return (
+        <>
+            <Header />
+            <CreateNote addElement={addElement} />
+            <div className='notes'>
+                {
+                    array.map((ele, index) => {
+                        return (
+                            <Note key={index} ele={ele} id={index} del={del} />
+                        )
+                    })
+                }
+            </div>
+            <Footer />
+        </>
+    )
+}
+
+export default MainPage
